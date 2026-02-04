abd368e35ef06a22e89cb8abad081fe05199af3a
olliecreatard <29oliver.creagan@boiseschools.net>
2026-02-03 19:12:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 function runCode() {
-  // 1. Hide the buttons
+  // 1. Instantly hide buttons
   var buttons = document.getElementById("buttonBox");
   buttons.style.display = "none";
   
   var text = document.getElementById("subTitle");
   text.style.display = "none";
 
-  // 2. Show the "Redeeming" message
+  // 2. Instantly show the waiting message
   var message = document.getElementById("waitMessage");
   message.style.display = "block";
 
-  // 3. Wait 3 seconds then show the prank
+  // 3. Wait 3 seconds...
   setTimeout(function() {
     
     // Hide the message
     message.style.display = "none";
 
-    // Show the box
+    // SNAP: Show the result box
     var box = document.getElementById("resultBox");
     box.style.display = "block";
 
-    // Change background to red
+    // SNAP: Change background to red immediately
     document.body.style.backgroundColor = "red";
 
     // Change title text
